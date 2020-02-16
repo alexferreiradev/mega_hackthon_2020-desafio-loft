3b77b009c21184499f6ec2b1064de038d2404975
Alex Rabelo Ferreira <arf92@live.com>
2020-02-16 01:05:05 -0300

    Alteracoes para dashboard
    - remocao de cards nao utilizados
    - adicao de componente para listar imóveis
    - adicao de style para dashboard

---
+++ frontend/src/views/dashboard/Imoveis/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Imoveis extends React.Component {
+
+    render() {
+        return (
+            <>
+                <ul>
+                    <li>
+                        Teste imovel 1
+                    </li>
+                    <li>
+                        Teste imovel 2
+                    </li>
+                    <li>
+                        Teste imovel 3
+                    </li>
+                </ul>
+            </>
+        );
+    }
+}
+
+export default Imoveis;
